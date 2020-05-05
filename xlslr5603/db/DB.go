d2008fa31129341c197c6c75a915277e9eb5d51d
richard <296907@qq.com>
2020-05-05 13:09:35 +0800

    build project to docker

--- xlslr5603/db/DB.go
+++ xlslr5603/db/DB.go
@@ -27,7 +27,7 @@ func NewDB() (db *gorm.DB, err error) {
 	} else {
 		err = godotenv.Load()
 		if err != nil {
-			log.Fatal("Error loading .env file")
+			log.Fatal("Error loading .env file: " + err.Error())
 			return nil, err
 		}
 	}
