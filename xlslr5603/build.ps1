b7f0dc6a006086528bf0873f17ed2f0a070c2c49
richard <296907@qq.com>
2020-05-06 17:46:54 +0800

    build project to docker

--- xlslr5603/build.ps1
+++ xlslr5603/build.ps1
@@ -3,7 +3,7 @@ git commit -m "build project to docker"
 git push
 
 docker run -it -v $PWD/:/go/src/github.com/wangsying/rfid/xlslr5603 -it golang go build -o /go/src/github.com/wangsying/rfid/xlslr5603/server /go/src/github.com/wangsying/rfid/xlslr5603/main.go
-Copy-Item server xlslr5603/docker
+Copy-Item server docker
 
 cd xlslr5603/docker
 docker build --no-cache -t registry.haier-ioc.com/rfid-xlslr560 .
