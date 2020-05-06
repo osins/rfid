166ce2a84cf6aeef587f09fec2e276ede7bcc5a0
richard <296907@qq.com>
2020-05-06 18:33:54 +0800

    build project to docker

--- xlslr5603/build.ps1
+++ xlslr5603/build.ps1
@@ -2,11 +2,12 @@ git add .
 git commit -m "build project to docker"
 git push
 
-cd docker
+Set-Location docker
+
 docker build --no-cache -t registry.haier-ioc.com/rfid-xlslr560 .
 docker push registry.haier-ioc.com/rfid-xlslr560
 
 docker tag registry.haier-ioc.com/rfid-xlslr560 wangsying/rfid-xlslr560
 docker push wangsying/rfid-xlslr560
 
-cd ..
+Set-Location ..
